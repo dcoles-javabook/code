3b6ef2f2015b9ba88936fe134dc8f2581a9b1dee
dcoles-javabook <243444915+dcoles-javabook@users.noreply.github.com>
2025-11-17 18:23:53 -0500

    Update StringDemo4.java

--- javabook/chap3/sect1/StringDemo4.java
+++ javabook/chap3/sect1/StringDemo4.java
@@ -1,21 +1,22 @@
 package javabook.chap3.sect1;
 
 /**
- * Demonstrates escape and Unicode characters.
+ * Demonstrates escape sequences and Unicode.
  *
  * @author Drue Coles
  */
 public class StringDemo4 {
 
    public static void main(String[] args) {
-      // escape characters
-      System.out.println("*\n**\n***");
-      System.out.println("T\tA\tB");
-      System.out.println("She said, \"Attack!\"");
-      System.out.println("Here is a backslash: \\");
+      System.out.println("SLUBBERDEGULLION");
+      System.out.println("SLUB\tBERD\nEGUL\tLION");
 
-      // Unicode
+      System.out.println("She said, \"Attack!\"");
+
+      System.out.println("Slash: /");
+      System.out.println("Backslash: \\");
+
       String symbols = "\u03c0 \u21c6 \u266c \u222c \u2665 \u2605";
-      System.out.println("Some Unicode characters: " + symbols);
+      System.out.println("Unicode: " + symbols);
    }
 }
